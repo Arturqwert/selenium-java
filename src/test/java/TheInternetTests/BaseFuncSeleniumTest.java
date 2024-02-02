b048436b24e85a6ec175c926592a1147ece232e7
Oliver <tkuap@bk.ru>
2024-02-02 21:51:06 +0300

    selenium base functions

---
+++ src/test/java/TheInternetTests/BaseFuncSeleniumTest.java
@@ -0,0 +1,123 @@
+package TheInternetTests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BaseFuncSeleniumTest {
+
+    private WebDriver driver;
+    private String baseUrl = "https://the-internet.herokuapp.com";
+    @BeforeEach
+    public void setUp()
+    {
+        ChromeOptions capabilities = new ChromeOptions();
+        capabilities.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        driver = new ChromeDriver(capabilities);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+    @Test
+    public void loginTest() {
+        driver.get(baseUrl + "/login");
+
+        driver.findElement(By.cssSelector("#username")).sendKeys("tomsmith");
+        driver.findElement(By.cssSelector("#password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector(".fa-sign-in")).click();
+
+        var res = driver.findElement(By.cssSelector("#flash")).getAttribute("class");
+
+        assertTrue(res.contains("success"));
+    }
+
+    @Test
+    public void longLoadingTest()
+    {
+        driver.get(baseUrl + "/dynamic_loading/2");
+
+        driver.findElement(By.cssSelector("#start button")).click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.of(6, ChronoUnit.SECONDS));
+
+        var text = driver.findElement(By.cssSelector("#finish")).getText();
+
+        assertEquals(text, "Hello World!");
+    }
+
+    @Test
+    public void switchBetweenTabs()
+    {
+        driver.get(baseUrl + "/windows");
+
+        driver.findElement(By.linkText("Click Here")).click();
+
+        var getHandles = driver.getWindowHandles();
+        String[] handles = new String[getHandles.size()];
+        getHandles.toArray(handles);
+
+        String firstTab = handles[0];
+        String secondTab = handles[1];
+
+        driver.switchTo().window(secondTab);
+
+        var text = driver.findElement(By.cssSelector("h3")).getText();
+
+        assertEquals(text, "New Window");
+    }
+
+    @Test
+    public void switchBetweenFrames()
+    {
+        driver.get(baseUrl + "/iframe");
+
+        driver.switchTo().frame("mce_0_ifr");
+
+        var textFromFrame = driver.findElement(By.cssSelector("#tinymce")).getText();
+        assertEquals(textFromFrame, "Your content goes here.");
+
+        driver.switchTo().defaultContent();
+
+        var textFromPage = driver.findElement(By.cssSelector("h3")).getText();
+        assertEquals(textFromPage, "An iFrame containing the TinyMCE WYSIWYG Editor");
+    }
+
+    @Test
+    public void authWithCookie() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileReader("src/main/resources/credentials.properties"));
+
+        String userToken = properties.getProperty("user.token");
+        driver.get("https://github.com");
+
+        driver.manage().addCookie(new Cookie("user_session", userToken));
+        driver.navigate().refresh();
+
+        var img = driver.findElement(By.cssSelector("span.Button-label img")).getAttribute("src");
+        driver.manage().deleteCookieNamed("user_session");
+
+        assertEquals(img, "https://avatars.githubusercontent.com/u/85057592?v=4");
+    }
+}
