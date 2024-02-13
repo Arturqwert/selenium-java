2a7010995738db1aa669c8e04e4779b46631bdae
Oliver <tkuap@bk.ru>
2024-02-13 21:40:02 +0300

    allure

--- src/test/java/PageFactory/LabirintTest.java
+++ src/test/java/PageFactory/LabirintTest.java
@@ -4,6 +4,7 @@ import PageFactory.Block.BookCard;
 import PageFactory.Page.MainPage;
 import PageFactory.Page.SearchResult;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,28 +20,37 @@ public class LabirintTest {
     private final String url = "https://www.labirint.ru/";
     private  SearchResult searchResult;
 
+    @DisplayName("Ищем книги на странице.")
     @Test
     public void searchPOMTest(ChromeDriver driver) throws InterruptedException {
-        searchResult = PageFactory.initElements(driver, SearchResult.class);
 
-        MainPage mainPage = new MainPage(driver);
+        throw new NullPointerException();
+//        searchResult = PageFactory.initElements(driver, SearchResult.class);
+//
+//        MainPage mainPage = new MainPage(driver);
+//
+//        mainPage.getWithConditions();
+//        mainPage.getHeader().search("java");
+//
+//        searchResult.sortByType("высокий рейтинг");
+//        searchResult.closeChip("ожидаются");
+//        searchResult.closeChip("нет в продаже");
+//
+//        List<BookCard> books = searchResult.getBooks();
+////
+////        for (BookCard book : books) {
+////            book.addToCart();
+////        }
+//        int i = 0;
+//        for (; i < 10; i++){
+//            books.get(i).addToCart();
+//        }
+//
+//        int booksCount = books.size();
+//        //int goodsInBasket = searchResult.getHeader().awaitLoadCartCounter(booksCount).getCartCounter();
+//
+//        //assertEquals(booksCount, goodsInBasket);
+//        assertEquals(i, 10, "Количество товаров в корзине не совпадает с фактичеким.");
 
-        mainPage.getWithConditions();
-        mainPage.getHeader().search("java");
-
-        searchResult.sortByType("высокий рейтинг");
-        searchResult.closeChip("ожидаются");
-        searchResult.closeChip("нет в продаже");
-
-        List<BookCard> books = searchResult.getBooks();
-
-        for (BookCard book : books) {
-            book.addToCart();
-        }
-
-        int booksCount = books.size();
-        int goodsInBasket = searchResult.getHeader().awaitLoadCartCounter(booksCount).getCartCounter();
-
-        assertEquals(booksCount, goodsInBasket);
     }
 }
